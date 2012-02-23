0ccda9b6c4a626519e5c77df3a6478a330f46046
Michael G. Schwern <schwern@pobox.com>
2012-02-23 00:27:54 -0800

    Big change to how the policy works, it now scans for the .foo VCS directories.
    I realized a command driven version wasn't going to work well
    * Windows users often use GUIs like TortiseSVN which have no command
    * Configuration for out of path commands would be PER USER and PER VCS not
      per project.  It would be a pain in the ass
    * It would be really slow as it would have to be done for every file
    * Possibility of side effects
    
    Now it just looks up the tree for the .foo VCS directory which covers
    most VCS.  Perforce and SVK get an automatic pass for now.  This also
    no longer requires the project to pre-configure the policy with the
    type of VCS in use, but you can still do that if you want.
    
    I also improved how it keeps it down to one check by telling PBP that
    this policy applies to the whole document.
    
    Still lacking real tests.

--- lib/Perl/Critic/Policy/Miscellanea/RequireVcs.pm
+++ lib/Perl/Critic/Policy/Miscellanea/RequireVcs.pm
@@ -11,6 +11,7 @@ use 5.006001;
 use strict;
 use warnings;
 use Readonly;
+use List::MoreUtils qw(uniq);
 
 use Perl::Critic::Utils qw{
     :booleans :characters :severities :data_conversion
@@ -22,24 +23,56 @@ our $VERSION = '1.117';
 
 #-----------------------------------------------------------------------------
 
+Readonly::Scalar my $DESC => q{The file does not appear to be under version control};
 Readonly::Scalar my $EXPL => [ 441 ];
 
 #-----------------------------------------------------------------------------
 
-Readonly::Scalar my $VCS_Checks => {
-    rcs         => \&_check_rcs,
-    cvs         => \&_check_cvs,
-    perforce    => \&_check_perforce,
-    svn         => \&_check_svn,
-    subversion  => \&_check_svn,
-    svk         => \&_check_svk,
-    git         => \&_check_git,
-    hg          => \&_check_mercurial,
-    mercurial   => \&_check_mercurial,
-    bazaar      => \&_check_bazaar,
-    darcs       => \&_check_darcs,
-    unlisted    => sub { return },
+# Identifying directories for various version control systems.
+# The policy will look up directories for them.
+Readonly::Scalar my $VCS_Dirs => {
+    cvs                 => "CVS",
+    subversion          => ".svn",
+    git                 => ".git",
+    mercurial           => ".hg",
+    bazaar              => ".bzr",
+    darcs               => "_darcs",
 };
+
+# Common aliases and abbreviations.
+Readonly::Scalar my $VCS_Aliases => {
+    svn         => "subversion",
+    hg          => "mercurial",
+    bzr         => "bazaar",
+    p4          => "perforce",
+};
+
+# Version control systems which may require special logic to identify.
+Readonly::Scalar my $VCS_Special_Checks => {
+    # Check for the RCS ,v file.
+    rcs         => sub {
+        my $self = shift;
+        my $vcs_file = $self->{_filename}.",v";
+        return -e $vcs_file || -e File::Spec->catfile("RCS", $vcs_file);
+    },
+};
+
+# A hash of VCS's we cannot check for.
+Readonly::Scalar my $Uncheckable_VCS => {
+    map { $_ => 1 } qw(
+        perforce
+        svk
+        unlisted
+    )
+};
+
+# A list of all the VCS names we recognize.
+Readonly::Scalar my $All_VCS_Names => [uniq(
+    keys   %$Uncheckable_VCS,
+    keys   %$VCS_Dirs,
+    keys   %$VCS_Special_Checks,
+    keys   %$VCS_Aliases
+)];
 
 sub supported_parameters {
     return (
@@ -48,88 +81,115 @@ sub supported_parameters {
             description         => 'The version control system in use.',
             default_string      => $EMPTY,
             behavior            => 'enumeration',
-            enumeration_values  => [ keys %$VCS_Checks ],
+            enumeration_values  => [ @$All_VCS_Names ],
             enumeration_allow_multiple_values => 0
-        },
-        {
-            name                => 'command_path',
-            description         => 'Path to the version control command.',
-            default_string      => $EMPTY,
-            behavior            => 'string',
-        },
+        }
     );
 }
 
-# This policy doesn't need to scan the document, so violation() only needs to
-# be called at most once.  Unfortunately you can't tell it to stop after
-# one *check*, only after one *violation*, so we also check if we've seen a doc
-# before.
-sub default_maximum_violations_per_document { return 1  }
+# We don't do anything with the document, but this makes the policy only
+# apply once per file.
+sub applies_to        { return 'PPI::Document' }
 
 sub default_severity  { return $SEVERITY_MEDIUM         }
 sub default_themes    { return qw(core pbp maintenance) }
 
 #-----------------------------------------------------------------------------
 
-sub initialize_if_enabled {
-    return $TRUE;
+sub _check_one_type {
+    my($self, $type) = @_;
+
+    return $self->_is_uncheckable($type)   ||
+           $self->_do_special_check($type) ||
+           $self->_look_up_dirs($type);
+
+    return $FALSE;
 }
 
-#-----------------------------------------------------------------------------
+sub _check_all_types {
+    my($self) = @_;
 
-sub _get_check {
+    return $self->_do_special_checks || $self->_look_up_dirs || $FALSE;
+}
+
+sub _is_uncheckable {
+    my($self, $type) = @_;
+
+    return $type if $Uncheckable_VCS->{$type};
+}
+
+sub _do_special_check {
+    my($self, $type) = @_;
+
+    my $check = $VCS_Special_Checks->{$type};
+    return $FALSE unless $check;
+
+    return $type if $self->$check();
+}
+
+sub _do_special_checks {
+    my($self) = @_;
+
+    for my $type (keys %$VCS_Special_Checks) {
+        return $type if $self->_do_special_check($type);
+    }
+
+    return $FALSE;
+}
+
+# Starting at the current directory, look up for
+# VCS directories.
+sub _look_up_dirs {
     my $self = shift;
 
-    my $type = $self->{_type};
-    return \&_missing_type unless $type;
+    my @types = @_ ? shift : keys %$VCS_Dirs;
 
-    my $check = $VCS_Checks->{$type};
-    return \&_invalid_type unless $check;
+    my $updir = File::Spec->updir;
+    my $dir   = $self->{_dir};
+    while( -d $dir ) {
+        for my $type (@types) {
+            return $self->_look_in_dir($type, $dir);
+        }
 
-    return $check;
+        # Up on dir
+        $dir = File::Spec->catdir( $updir, $dir );
+    }
+
+    return $FALSE;
 }
 
-sub _missing_type {
-    my $self = shift;
-    my $elem = shift;
+sub _look_in_dir {
+    my( $self, $type, $dir ) = @_;
 
-    my $desc = "Version control type not specified, please set 'type' in your .perlcriticrc";
-    return $self->violation( $desc, $EXPL, $elem );
+    my $vcs_dir = $VCS_Dirs->{$type};
+    return $type if -d File::Spec->catdir($dir, $vcs_dir);
+    return;
 }
-
-sub _invalid_type {
-    my $self = shift;
-    my $elem = shift;
-
-    my $type = $self->{_type};
-    my $desc = "'$type' is not a recognized version control system.  Consider using the 'unlisted' type";
-    return $self->violation( $desc, $EXPL, $elem );
-}
-
-
-sub _seen_doc {
-    my( $self, $doc ) = @_;
-
-    my $seen_doc = $self->{_docs_seen}{$doc};
-    $self->{_docs_seen}{$doc}++;
-
-    return $seen_doc;
-}
-
 
 sub violates {
     my( $self, $elem, $doc ) = @_;
 
-    # We've already checked this document, no need to do it again.
-    return if $self->_seen_doc($doc);
+    $DB::single = 1;
 
-    my $check = $self->_get_check;
+    # Get the filename we're examining.
+    $self->{_filename} = $doc->filename;
 
-    my $violation = $self->$check($doc);
+    # Derive the directory the file lives in.
+    my @path = File::Spec->splitpath( $self->{_filename } );
+    pop @path;
+    $self->{_dir} = File::Spec->catpath(@path, "");
 
-    return unless $violation;
-    return $violation;
+    if( my $type = $self->{_type} ) {
+        return if $self->_check_one_type($type);
+    }
+    else {
+        return if $self->_check_all_types;
+    }
+
+    return $self->violation( $DESC, $EXPL, $doc );
 }
+
+1;
 
 __END__
 
